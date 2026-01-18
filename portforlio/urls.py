6bc1643dc7845ecbdbe9669fd558bfd8755b4f32
elbangstellyn <elbangstellyn@gmail.com>
2026-01-18 18:20:58 +0100

    fixed image not showing

--- portforlio/urls.py
+++ portforlio/urls.py
@@ -1,8 +1,41 @@
+# """
+# URL configuration for portforlio project.
+
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/6.0/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# from django.contrib import admin
+# from django.urls import path,include
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+# urlpatterns = [
+#     path('admin/att/', admin.site.urls),
+#     path('', include('portforlio_app.urls'))
+# ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
+
+
 """
 URL configuration for portforlio project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/  
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,14 +48,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/att/', admin.site.urls),
-    path('', include('portforlio_app.urls'))
+    path('', include('portforlio_app.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# Serve media files in both development and production (like on Render)
+# Note: This is less efficient than serving via a webserver like Nginx,
+# but it works for platforms like Render with ephemeral storage.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve static files collected by collectstatic (including built-in Django admin static files)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
