c19a04efe61c42bcac0388e53184305f4ba5d64d
elbangstellyn <elbangstellyn@gmail.com>
2026-02-13 04:08:43 +0100

    new project added

--- portforlio_app/models.py
+++ portforlio_app/models.py
@@ -9,6 +9,8 @@ class PortforlioDetail(models.Model):
     title = models.CharField(max_length=150)
     body = models.TextField()
     image = CloudinaryField('image')
+    def __str__(self):
+        return self.title
 
 class Projects(models.Model):
     title = models.CharField(max_length=150)
@@ -16,23 +18,33 @@ class Projects(models.Model):
     image = CloudinaryField('image')
     link = models.CharField(max_length=300, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
 
 class Stack(models.Model):
     name = models.CharField(max_length=150)
     image = CloudinaryField('image')
+    def __str__(self):
+        return self.name
 
 class Framework(models.Model):
     name = models.CharField(max_length=150)
     image = CloudinaryField('image')
+    def __str__(self):
+        return self.name
 
 class About(models.Model):
     title = models.CharField(max_length=150)
     image = CloudinaryField('image')
     body = models.TextField()
+    def __str__(self):
+        return self.title
 
 class Cv(models.Model):
     name = models.CharField(max_length=150)
     cv = CloudinaryField('raw')
+    def __str__(self):
+        return self.name
 
 
     
